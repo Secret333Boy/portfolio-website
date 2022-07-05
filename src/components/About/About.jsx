6ca40c979162ed699e26ee12cdbe3fedb6c708d6
Eugene <pashkovskiy.eugen@gmail.com>
2022-07-05 20:11:16 +0300

    Education component implemented

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -35,7 +35,10 @@ const About = () => {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
           illo, fugit officiis mollitia accusamus asperiores blanditiis earum,
           velit ratione natus numquam debitis vel animi iusto illum facere,
-          incidunt ex! Delectus?
+          incidunt ex! Delectus? Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Architecto, doloremque, aperiam corporis sint
+          nostrum quod fugit nihil placeat est aliquid vitae quaerat dolorum
+          debitis beatae voluptatem. At delectus aut numquam?
         </p>
       </Card>
     </section>
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Card, Avatar, Divider } from '@mui/material';
+import uniLogo from './img/NTUU_KPI_logo.png';
 
 const Education = () => {
   return (
-    <section id="education">
+    <section
+      id="education"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        padding: '1em',
+      }}
+    >
       <Typography variant="h2">Education</Typography>
+      <Card
+        elevation={2}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          maxWidth: '50vw',
+          p: 1.5,
+        }}
+      >
+        <Avatar src={uniLogo} sx={{ width: 150, height: 150 }} />
+        <Divider
+          variant="middle"
+          orientation="vertical"
+          flexItem
+          sx={{ ml: 1, mr: 1 }}
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          optio fugiat minima esse iure facilis et, inventore doloremque ea
+          excepturi eaque, provident aperiam voluptatibus, expedita libero
+          reiciendis. Corrupti, culpa quam! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Sequi, quisquam quaerat numquam
+          provident ratione cupiditate aspernatur in totam. In qui ut tempore a
+          quis ad, nam beatae nisi! Neque, quia.
+        </p>
+      </Card>
     </section>
   );
 };
